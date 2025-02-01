df092f9c9d700244187fb4f45e3f4e7501b999bd
Shubham Babhulkar <babhulkar.shubham03@gmail.com>
2025-02-01 23:16:35 +0530

    did curd operations

--- src/app.js
+++ src/app.js
@@ -1,15 +1,54 @@
 const express = require("express");
-
 const app = express();
-app.use("/hi",(req, res) => {
-    res.send('Hi Hi Hi Hi');
+
+
+app.get("/user",(req, res) => {
+    res.send({firstName:"Shubhanm", lastName:"Babhulkar"});
 })
-app.use("/hello",(req, res) => {
-    res.send('hello hello hello hello 12355');
+
+app.post("/user",(req, res) => {
+    //Your data saving logic should be here
+    res.send({message: "Data Save Successfully", status: true});
 })
-app.use("/data",(req, res) => {
-    res.send('Hi From the Shubham');
+
+app.delete("/user",(req, res) => {
+    res.send("User data deleted Successfully");
 })
+
+app.put("/user",(req, res) => {
+    res.send("Put the Data Successfully")
+})
+
+app.patch("/user", (req, res) => {
+    res.send("Path the data successfully");
+})
+
+
+// app.use("/hi",(req, res) => {
+//     res.send('Hi Hi Hi Hi');
+// })
+
+// app.get("/hello",(req, res) => {
+//     res.send('hello hello hello hello');
+// })
+// app.use("/hello/shubham",(req, res) => {
+//     res.send('hello Shubham');
+// })
+
+
+// app.use("/data",(req, res) => {
+//     res.send('Hi From the Shubham');
+// })
+
+// app.use((req, res) => {
+//     res.send('Namaste Node from server');
+// })
+
+//Wild card route go in side below route when route not match with any
+app.use("/", (req, res) => {
+    res.send('Route is not match');
+})
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
